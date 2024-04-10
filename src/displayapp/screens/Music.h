499b9f76921c3fb555826673005fb51e6c2e877e
Kevindud3 <122136657+Kevindud3@users.noreply.github.com>
2024-04-10 12:29:54 -0300

    Update Music.h

--- src/displayapp/screens/Music.h
+++ src/displayapp/screens/Music.h
@@ -36,7 +36,7 @@ namespace Pinetime {
     namespace Screens {
       class Music : public Screen {
       public:
-        Music(Pinetime::Controllers::MusicService& music);
+        Music(Controllers::MotorController& motorController, Pinetime::Controllers::MusicService& music);
 
         ~Music() override;
 
